643f07ef2a8afe9a7d8d8cbefde96ba3117cfa5a
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-01-22 23:12:30 +0700

    Add homebase image below Maps

--- lib/menu_pages/about_us.dart
+++ lib/menu_pages/about_us.dart
@@ -68,31 +68,67 @@ class _AboutUsState extends State<AboutUs> {
                     ],
                   ),
                   Center(
-                    child: Card(
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 300,
-                            child: GoogleMap(
-                              mapType: MapType.normal,
-                              initialCameraPosition: CameraPosition(
-                                  target: LatLng(
-                                      -6.895327499970892, 106.80946383829516),
-                                  zoom: 14),
-                              markers: _marker,
-                              onTap: (position) {
-                                setState(() {
-                                  _marker.add(Marker(
-                                      markerId: MarkerId(
-                                          "${position.latitude}, ${position.longitude}"),
-                                      icon: BitmapDescriptor.defaultMarker,
-                                      position: position));
-                                });
-                              },
-                            ),
+                    child: Column(
+                      children: [
+                        Card(
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 300,
+                                child: GoogleMap(
+                                  mapType: MapType.normal,
+                                  initialCameraPosition: CameraPosition(
+                                      target: LatLng(-6.895327499970892,
+                                          106.80946383829516),
+                                      zoom: 14),
+                                  markers: _marker,
+                                  onTap: (position) {
+                                    setState(() {
+                                      _marker.add(Marker(
+                                          markerId: MarkerId(
+                                              "${position.latitude}, ${position.longitude}"),
+                                          icon: BitmapDescriptor.defaultMarker,
+                                          position: position));
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(
+                          height: 20
+                        ),
+                        SizedBox(
+                          child: Text(HOMEBASE,
+                              textAlign: TextAlign.left,
+                              style: GoogleFonts.lobsterTwo(
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.amber[400],
+                                  fontSize: 16)),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(
+                                top: 10.0, bottom: 10, left: 30, right: 30),
+                            height: 500,
+                            width: 600,
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                    color: Colors.black38, width: 1.0),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          "assets/images/homebase1.jpg"),
+                                      fit: BoxFit.cover),
+                                ),
+                              ),
+                            ))
+                      ],
                     ),
                   ),
                 ],
