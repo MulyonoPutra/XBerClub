31e514dae059c291082b3563dee97f760d715c97
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-01-22 22:48:46 +0700

    Implements Push Notification using Google Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,53 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:xber_futsal_club_app/collapse_appbar.dart';
 
+import 'mocks/messages.dart';
 
 void main() {
   runApp(XberApp());
 }
 
-class XberApp extends StatelessWidget {
+class XberApp extends StatefulWidget {
+  @override
+  _XberAppState createState() => _XberAppState();
+}
+
+class _XberAppState extends State<XberApp> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final List<Messages> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        final notification = message['notification'];
+        setState(() {
+          messages.add(Messages(
+              title: notification['title'], body: notification['body']));
+        });
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+
+        final notification = message['data'];
+        setState(() {
+          messages.add(Messages(
+            title: '${notification['title']}',
+            body: '${notification['body']}',
+          ));
+        });
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,4 +56,3 @@ class XberApp extends StatelessWidget {
     );
   }
 }
-
---
+++ lib/mocks/messages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Messages {
+  final String title;
+  final String body;
+
+  const Messages({@required this.title, @required this.body});
+
+}
